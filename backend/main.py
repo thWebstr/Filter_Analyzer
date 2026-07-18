5bcbc3ce2a38481240518d6a4444fbefc19d80fb
Adeniran Moses <thewebstr25@gmail.com>
2026-07-18 05:53:09 +0100

    fix: adapt backend for vercel zero-config python serverless expectations

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes import router
+from routers.routes import router
 
 app = FastAPI(
     title="FilterAnalyzer API",
